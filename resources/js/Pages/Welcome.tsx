5b42eda8a10ac341ebf88f89b950f7fff867e4c5
mviner000 <121312294+mviner000@users.noreply.github.com>
2024-04-06 14:50:10 +0800

    03

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -99,7 +99,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps<
                                     </div>
 
                                     <div className="pt-3 sm:pt-5">
-                                        <h2 className="text-xl font-semibold text-black dark:text-white">Hindi pa pala, 01</h2>
+                                        <h2 className="text-xl font-semibold text-black dark:text-white">Hindi pa pala, 03</h2>
 
                                         <p className="mt-4 text-sm/relaxed">
                                             Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
