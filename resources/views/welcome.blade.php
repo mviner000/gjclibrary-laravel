5858b9d59ab62e6790368223b80f78f0b0085ae9
mviner000 <121312294+mviner000@users.noreply.github.com>
2024-04-06 14:40:40 +0800

    Inayos ko ang git pull

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -111,10 +111,10 @@
                                 </div>
 
                                 <div class="pt-3 sm:pt-5">
-                                    <h2 class="text-xl font-semibold text-black dark:text-white">Laracasts</h2>
+                                    <h2 class="text-xl font-semibold text-black dark:text-white">Okay na ang Github Pull</h2>
 
                                     <p class="mt-4 text-sm/relaxed">
-                                        Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
+                                        Hey of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
                                     </p>
                                 </div>
 
