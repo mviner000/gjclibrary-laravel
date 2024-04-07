495e6f60d16ff2b4183f374a0868083024707cda
mviner000 <121312294+mviner000@users.noreply.github.com>
2024-04-07 20:00:53 +0800

    route updated 8pm 4-7-24

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::redirect('/', 'dashboard');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', fn() => Inertia::render('Dashboard'))
